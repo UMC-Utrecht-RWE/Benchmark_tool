07e101788dd332f2a48b9a3d779acb879408692f
T. Andres Vaz <t.andresvaz@umcutrecht.nl>
2022-06-24 14:31:19 +0200

    initial commit

---
+++ main.R
@@ -0,0 +1,40 @@
+#Author: VAZ, TA
+#tandresv@umcutrecht.nl
+#University Medical Center Utrecht
+#10/6/2022
+#CoVE - time performance benchmarking
+
+#start
+rm(list=ls())
+
+#settings
+projectFolder<-dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(projectFolder)
+source("./packages.R")
+
+#benchmarking: start clock
+start_time <- Sys.time()
+
+#mockup function to verify if result from different processes are identical
+check_equal <- function(values) {
+  val1 <- 1
+  val2 <- 1
+  val2 == val1
+}
+pre_dir <- paste0(projectFolder,"/source/p_steps/")
+#execute different implementations for the same problem 100x each
+mbm <- microbenchmark( "a" = { a <- 1+2 },
+                       "b" = { b <- 1+2 },
+                       # "c" = { c <- source(paste0(pre_dir, "/function_1.R")) },
+                       check = check_equal,
+                       times = 100
+)
+
+#print results
+library(ggplot2)
+mbm
+autoplot(mbm)
+end_time <- Sys.time()
+end_time - start_time
+
+
---
+++ packages.R
@@ -0,0 +1,21 @@
+#Packages usually needed
+if(!require(stringr)){install.packages("stringr")}
+suppressPackageStartupMessages(library(stringr))
+if(!require(dplyr)){install.packages("dplyr")}
+suppressPackageStartupMessages(library(dplyr))
+if(!require(data.table)){install.packages("data.table")}
+suppressPackageStartupMessages(library(data.table))
+if(!require(lubridate)){install.packages("lubridate")}
+suppressPackageStartupMessages(library(lubridate))
+if(!require(tidyverse)){install.packages("tidyverse")}
+suppressPackageStartupMessages(library(tidyverse))
+if(!require(tidyselect)){install.packages("tidyselect")}
+suppressPackageStartupMessages(library(tidyselect))
+if(!require(reshape)){install.packages("reshape")}
+suppressPackageStartupMessages(library(reshape))
+if(!require(RPostgres)){install.packages("RPostgres")}
+suppressPackageStartupMessages(library(RPostgres))
+if(!require(microbenchmark)){install.packages("microbenchmark")}
+suppressPackageStartupMessages(library(microbenchmark))
+if(!require(rstudioapi)){install.packages("rstudioapi")}
+suppressPackageStartupMessages(library(rstudioapi))
